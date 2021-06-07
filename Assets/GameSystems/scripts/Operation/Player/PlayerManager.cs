51f656a26c7c450a01973c17b0d3d8f914d41ae8
kurihiro <kuriharahironari@gmail.com>
2021-06-07 10:35:41 +0900

    add animation

--- Assets/GameSystems/scripts/Operation/Player/PlayerManager.cs
+++ Assets/GameSystems/scripts/Operation/Player/PlayerManager.cs
@@ -4,15 +4,44 @@ using UnityEngine;
 
 public class PlayerManager : MonoBehaviour
 {
-    // Start is called before the first frame update
+    float speed;
+    float speed_y;
+    new Rigidbody2D rigidbody2D;
+    Animator animator;
+
+
+    // Start is called before the firstx  frame update
     void Start()
     {
-        
+        rigidbody2D = GetComponent<Rigidbody2D>();
+        animator = GetComponent<Animator>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        float x = Input.GetAxis("Horizontal");
+        float y = Input.GetAxis("Vertical");
+
+        animator.SetFloat("speed_x", x);
+        animator.SetFloat("speed_y", y);
+
+        if (x == 0)
+        {
+            speed = 0;
+        }
+        else if (x > 0)
+        {
+            speed = 3;
+            transform.localScale = new Vector3(1, 1, 1);
+        }
+        else if (x < 0)
+        {
+            speed = -3;
+            transform.localScale = new Vector3(-1, 1, 1);
+        }
+
+        rigidbody2D.velocity = new Vector2(speed, rigidbody2D.velocity.y);
+
     }
 }
